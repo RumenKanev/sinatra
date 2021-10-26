0a3f294aa58221a6576c0470027ad1d1c19f0cfa
RumenKanev <rkynev@gmail.com>
2021-10-26 14:10:17 +0100

    sinatra_keeping_views_clean

--- app.rb
+++ app.rb
@@ -1,15 +1,8 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-get '/' do
-    "Hello World!"
-end
-
-get '/secret' do 
-    "Sweet Dreams!!!!!"
-end
 
 get '/cat' do 
+    @name = ["Amigo", "Misty", "Almond"].sample
     erb(:index)
-    
 end
