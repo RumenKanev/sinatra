9580c2eb8f9766d4988c4559c8f30230e403d30b
RumenKanev <rkynev@gmail.com>
2021-10-26 09:10:18 +0100

    sinatra_returning_html

---
+++ app.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+get '/' do
+    "Hello World!"
+end
+
+get '/secret' do 
+    "Sweet Dreams!!!!!"
+end
+
+get '/cat' do 
+    "<div style='border: 3px dashed red'>
+        <img src='http://placekitten.com/500/500'>
+     </div>"
+end
